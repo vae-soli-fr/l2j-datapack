d58bd60ec666abf4d5018ee813db91d7357c25af
Melua <melua@melua.fr>
2020-08-03 20:49:54 +0200

    Add fashion handler

--- dist/game/data/scripts/handlers/voicedcommandhandlers/Fashion.java
+++ dist/game/data/scripts/handlers/voicedcommandhandlers/Fashion.java
@@ -1,9 +1,15 @@
 package handlers.voicedcommandhandlers;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.l2jserver.gameserver.CustomImage;
+import com.l2jserver.gameserver.cache.HtmCache;
 import com.l2jserver.gameserver.handler.IVoicedCommandHandler;
 import com.l2jserver.gameserver.model.actor.instance.L2PcInstance;
 import com.l2jserver.gameserver.model.itemcontainer.Inventory;
 import com.l2jserver.gameserver.model.items.instance.L2ItemInstance;
+import com.l2jserver.gameserver.network.serverpackets.NpcHtmlMessage;
 
 public class Fashion implements IVoicedCommandHandler {
 	private static final String[] VOICED_COMMANDS = {
@@ -11,25 +17,46 @@ public class Fashion implements IVoicedCommandHandler {
 		"dressme"
 	};
 
-	private static final int[] PAPERDOLL_ALLOWED = new int[] {
-			Inventory.PAPERDOLL_UNDER,
-			Inventory.PAPERDOLL_HEAD,
-			Inventory.PAPERDOLL_GLOVES,
-			Inventory.PAPERDOLL_CHEST,
-			Inventory.PAPERDOLL_LEGS,
-			Inventory.PAPERDOLL_FEET,
-			Inventory.PAPERDOLL_RHAND,
-			Inventory.PAPERDOLL_LHAND
-		};
-
 	@Override
 	public boolean useVoicedCommand(String command, L2PcInstance activeChar, String params) {
 
-		final int expertiseLevel = activeChar.getExpertiseLevel() + 1;
+		List<Integer> updatedSlots = new ArrayList<>();
 
-		for (int slot : PAPERDOLL_ALLOWED) {
+		switch(String.valueOf(params)) {
+			case "full":
+				updatedSlots.add(Inventory.PAPERDOLL_HEAD);
+				updatedSlots.add(Inventory.PAPERDOLL_CHEST);
+				updatedSlots.add(Inventory.PAPERDOLL_LEGS);
+				updatedSlots.add(Inventory.PAPERDOLL_GLOVES);
+				updatedSlots.add(Inventory.PAPERDOLL_FEET);
+				updatedSlots.add(Inventory.PAPERDOLL_RHAND);
+				updatedSlots.add(Inventory.PAPERDOLL_LHAND);
+				break;
+			case "chest":
+				updatedSlots.add(Inventory.PAPERDOLL_CHEST);
+				break;
+			case "legs":
+				updatedSlots.add(Inventory.PAPERDOLL_LEGS);
+				break;
+			case "gloves":
+				updatedSlots.add(Inventory.PAPERDOLL_GLOVES);
+				break;
+			case "feet":
+				updatedSlots.add(Inventory.PAPERDOLL_FEET);
+				break;
+			case "hands":
+				updatedSlots.add(Inventory.PAPERDOLL_RHAND);
+				updatedSlots.add(Inventory.PAPERDOLL_LHAND);
+				break;
+			default:
+				activeChar.sendMessage("Usage: .fashion <full|chest|legs|gloves|feet|hands>");
+				showMenu(activeChar);
+				return false;
+		}
+
+		for (int slot : updatedSlots) {
 			L2ItemInstance item = activeChar.getInventory().getPaperdollItem(slot);
-			if (item == null || item.getItem().getCrystalType().getId() > expertiseLevel) {
+			if (item == null) {
 				activeChar.setFashionItem(slot, 0);
 			} else {
 				activeChar.setFashionItem(slot, item.getDisplayId());
@@ -39,11 +66,22 @@ public class Fashion implements IVoicedCommandHandler {
 		activeChar.broadcastUserInfo();
 		activeChar.sendMessage("Equipement enregistre.");
 
+		showMenu(activeChar);
 		return true;
 	}
 
 	@Override
 	public String[] getVoicedCommandList() {
 		return VOICED_COMMANDS;
+	}
+
+	public void showMenu(L2PcInstance activeChar) {
+		String fashionMenu = HtmCache.getInstance().getHtm(activeChar.getHtmlPrefix(), "data/html/fashion.htm");
+		if (fashionMenu != null)
+		{
+			NpcHtmlMessage html = new NpcHtmlMessage(fashionMenu);
+			CustomImage.sendPackets(activeChar, html);
+			activeChar.sendPacket(html);
+		}
 	}
 }
