c78b16927d1297c790319933b55c47763d89dd8b
David Kühner <dav.kuhn@gmail.com>
2022-10-08 22:30:07 +0200

    Improve description handler

--- dist/game/data/scripts/handlers/voicedcommandhandlers/Description.java
+++ dist/game/data/scripts/handlers/voicedcommandhandlers/Description.java
@@ -10,6 +10,11 @@ public class Description implements IVoicedCommandHandler {
 	private static final String[] VOICED_COMMANDS = {
 		"desc"
 	};
+	public static final String NO_TITLE = ".                                                                                ";
+	private static final String HTML_TEMPLATE = "<html><title>" + NO_TITLE + "</title><body>" +
+												"<table border=0 cellpadding=10 cellspacing=0 height=355 width=235 background=\"L2UI_CH3.refinewnd_back_Pattern\">" +
+												"<tr><td><center><br><font name=\"hs12\">%s</font><br></center>%s<br><br><br></td></tr>" +
+												"</table></body></html>";
 
 	@Override
 	public boolean useVoicedCommand(String command, L2PcInstance activeChar, String params) {
@@ -25,12 +30,21 @@ public class Description implements IVoicedCommandHandler {
 			activeChar.sendMessage(player.getName() + " n'a pas encore de description.");
 		} else {
 			NpcHtmlMessage html = new NpcHtmlMessage();
-			html.setHtml("<html><title>" + target.getName() + "</title><body><br>" + escapeXml(player.getDescription()) + "</body></html>");
+			String descriptionHtml = String.format(HTML_TEMPLATE, target.getName(), escapeXml(player.getDescription()));
+			descriptionHtml = replaceTags(descriptionHtml);
+			html.setHtml(descriptionHtml);
 			CustomImage.sendPackets(activeChar, html);
 			activeChar.sendPacket(html);
 		}
 
 		return true;
+	}
+
+	private String replaceTags(String descriptionHtml) {
+		return descriptionHtml
+				.replace("[br]","<br>")
+				.replace("[center]", "<center>")
+				.replace("[/center]", "</center>");
 	}
 
 	@Override
@@ -41,4 +55,4 @@ public class Description implements IVoicedCommandHandler {
 	public static String escapeXml(String s) {
 	    return s.replaceAll("&", "&amp;").replaceAll(">", "&gt;").replaceAll("<", "&lt;");
 	}
-}
+}
